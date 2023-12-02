7d4d350b91387b750432b182c6ff4ee013feb106
taka-4602 <116678390+taka-4602@users.noreply.github.com>
2023-12-03 04:25:14 +0900

    Add files via upload

---
+++ hided online detector.py
@@ -0,0 +1,41 @@
+import discord
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = discord.Client(intents=discord.Intents.all())
+tree = discord.app_commands.CommandTree(client)
+
+TOKEN = 'Botのトークンをここに'
+
+@client.event
+async def on_message(message: discord.Message):  
+
+    if message.author.bot:
+        return
+    if "offline" in message.author.status:
+        embed = discord.Embed(title="⚠️オンライン非表示を検出しました⚠️", description=message.author.display_name,color=discord.Colour.red())
+        try:
+            embed.set_thumbnail(url=message.author.display_avatar.url)
+        except:
+            None
+        await message.channel.send(embed=embed)
+
+    if message.content=="spo!":
+        member=message.author
+        try:
+            if member.activity.title:
+                embed = discord.Embed(title="Spotify", description=f"### [{member.activity.title}]({member.activity.track_url})\n{member.activity.artist}",color=discord.Colour.green())
+                embed.set_thumbnail(url=member.activity.album_cover_url)
+                try:          
+                    embed.set_footer(text=member.display_name,icon_url=member.display_avatar.url)
+                except:
+                    None
+                await message.channel.send(embed=embed)
+        except:
+            await message.channel.send("あなたはSpotifyを聴いていません")
+
+@client.event
+async def on_ready():
+    print(f'Thank you for running! {client.user}')
+    
+client.run(TOKEN)
